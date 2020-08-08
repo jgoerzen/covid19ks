ebbcbb5d5759ab66baaf49d4ece2919e16e53f21
John Goerzen <jgoerzen@complete.org>
2020-08-08 11:46:07 -0500

    builds

--- src/charts.rs
+++ src/charts.rs
@@ -34,9 +34,16 @@ pub fn write(masks: &HashMap<NaiveDate, ARecord>, nomasks: &HashMap<NaiveDate, A
         .x_label_area_size(40)
         .y_label_area_size(40)
         .caption("Caption 1", ("sans-serif", 50.0).into_font())
-        .build_ranged(n2d(&datelist[0])..n2d(datelist.last().unwrap()), 0f32..500f32).unwrap();
+        .build_ranged(n2d(&datelist[0])..n2d(datelist.last().unwrap()), 0f64..500f64).unwrap();
 
     chart.configure_mesh().line_style_2(&WHITE).draw().expect("draw");
+    let series: Vec<(NaiveDate, f64)> = datelist.iter().map(|d| (d.clone(), masks.get(d).unwrap().newcaseavg)).collect();
+    chart.draw_series(LineSeries::new(
+        datelist.iter().map(|d| (n2d(d), masks.get(d).unwrap().newcaseavg)),
+            &BLUE
+    )).unwrap();
+
+   
 
 
 }
